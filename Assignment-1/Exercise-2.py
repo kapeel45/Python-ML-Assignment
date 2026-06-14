1bd14294ee330e7cc317071ce7a0733ec8de56b8
K Mac <mac@Macs-MacBook-Pro.local>
2026-06-14 09:55:09 +0530

    second  exercise commit

---
+++ Assignment-1/Exercise-2.py
@@ -0,0 +1,20 @@
+# Addresss of a and b is same because both are pointing to same object in memory. In python, small integers are cached and reused, so when you assign the value 10 to both a and b, they both reference the same integer object in memory.
+
+a = 10
+b = 10
+
+print("a Type is ::", type(a))
+print("a Address is ::", id(a))
+
+print("b Type is ::", type(b))
+print("b Address is ::", id(b))
+
+# While, list is mutable object, so when you create two separate lists with the same value, they are stored in different memory locations.
+a = [10]
+b = [10]
+
+print("a Type is ::", type(a))
+print("a Address is ::", id(a))
+
+print("b Type is ::", type(b))
+print("b Address is ::", id(b))
